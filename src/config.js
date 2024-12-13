7b887e7c227ef4a8fd83a424cca8865a56d94d41
jerzy szajner <jerzy.szajner@gmail.com>
2024-12-13 12:12:55 +0100

    Fix: update API URL for production on Netlify

--- src/config.js
+++ src/config.js
@@ -1,1 +1,6 @@
-export const API_URL = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:3131/api';
+// export const API_URL =
+//   process.env.NODE_ENV === "production" ? "/api" : "http://localhost:3131/api";
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://waiter-app-rbef.onrender.com/api"
+    : "http://localhost:3131/api";
